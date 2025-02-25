720ecb12a0e9bf9af1f380451d730449dafc4e29
shiori shimohira <sok.918.mail@gmail.com>
2025-02-25 15:09:27 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const path = require("path");
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, "public")))
+
+// app.get('/', function (req, res) {
+//     console.log(req);
+//     res.send('<h1>トップページ!</h1>')
+// })
+
+app.post('/api/v1/quiz', function (req, res) {
+
+    answer = req.body.answer;
+
+    if ( answer === "2") {
+        // res.send("正解");
+        res.redirect("/correct.html");
+    } else {
+        // res.send("不正解");
+        res.redirect("/wrong.html");
+    }
+
+    res.send(answer)
+  })
+
+app.get('/api/v1/users', function (req, res) {
+    res.send({
+        name: "Mike",
+        age:30,
+    })
+  })
+
+app.listen(3000, function() {
+    console.log("I am running");
+});
